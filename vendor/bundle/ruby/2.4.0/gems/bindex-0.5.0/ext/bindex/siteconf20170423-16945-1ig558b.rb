7099c115c8879319710e334f2f653ce2d0034104
ryu-yama <ryu.treasure628@gmail.com>
2017-04-24 10:04:52 +0100

    Initialize repository

---
+++ vendor/bundle/ruby/2.4.0/gems/bindex-0.5.0/ext/bindex/siteconf20170423-16945-1ig558b.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20170423-16945-13jxk7v"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
